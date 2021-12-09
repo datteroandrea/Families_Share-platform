e2d4c4603325ab3699aba9d12cc8cbb4b9fb638d
datteroandrea <dattero.andrea@gmail.com>
2021-12-09 17:50:10 +0100

    added FriendshipRequestsScreen

--- client/src/components/FriendshipRequestsScreen.js
+++ client/src/components/FriendshipRequestsScreen.js
@@ -1,22 +1,90 @@
 import React from "react";
+import axios from "axios";
 import withLanguage from "./LanguageContext";
-import BackNavigation from "./BackNavigation";
+import PropTypes from "prop-types";
 import Texts from "../Constants/Texts";
 import FriendshipsNavbar from "./FriendshipsNavbar"
+import Log from "./Log";
+import ProfilesAutoComplete from "./ProfilesAutoComplete";
+import UserList from "./UserList";
 
-const FriendshipRequestsScreen = ({ history, language }) => {
-  const handleBackNav = () => {
-    history.goBack();
+class FriendshipRequestsScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fetchedUsers: false,
+      profileId: "",
+      searchInput: "",
+      searchedForInput: false,
+      searchBarIsVisible: false,
+      requests: []
+    };
+  }
+  componentDidMount() {
+    const { match } = this.props;
+    const { profileId } = match.params;
+    const requests = [];
+    axios
+      .get(`/api/friends/${profileId}/requests`)
+      .then(res => {
+        console.log(res.data);
+        res.data.forEach(request => {
+          requests.push(request);
+        });
+        this.setState({ fetchedUsers: true, profileId: profileId, requests: requests });
+      })
+      .catch(error => {
+        Log.error(error);
+        this.setState({ fetchedUsers: true });
+      });
+  }
+
+  render() {
+    const { language, history } = this.props;
+    const {
+      fetchedUsers,
+      profileId,
+      searchBarIsVisible,
+      searchInput,
+      searchedForInput,
+      requests
+    } = this.state;
+    const texts = Texts[language].searchUserScreen;
+    return (
+      fetchedUsers && (
+        <React.Fragment>
+          <div className="row no-gutters" id="searchUserBarContainer">
+            <div className="col-2-10">
+              <button
+                type="button"
+                className="transparentButton center"
+                onClick={() => history.replace("/myfamiliesshare")}
+              >
+                <i className="fas fa-arrow-left" />
+              </button>
+            </div>
+            <div
+              className="col-7-10 "
+              style={{ display: "flex", alignItems: "center" }}
+            >
+            </div>
+          </div>
+          <div>
+            <div className="row no-gutters" id="searchUserResultsContainer">
+              <h1></h1>
+            </div>
+            <UserList userIds={requests} />
+          </div>
+          <FriendshipsNavbar allowNavigation={true} profileId={profileId} />
+        </React.Fragment >
+      )
+    );
   };
-  const texts = Texts[language].myFriendshipsRequestsScreen;
+}
 
-  return (
-    <React.Fragment>
-      <BackNavigation title={texts.backNavTitle} onClick={handleBackNav} />
-      {/* lista richieste */}
-      <FriendshipsNavbar allowNavigation={true}/>
-    </React.Fragment>
-  );
+FriendshipRequestsScreen.propTypes = {
+  language: PropTypes.string,
+  history: PropTypes.object
 };
 
 export default withLanguage(FriendshipRequestsScreen);
