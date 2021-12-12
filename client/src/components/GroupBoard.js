6853cd844af46824846dd7712351cfce9fc67c2d
Simojoviz <simonejov@gmail.com>
2021-12-12 12:15:40 +0100

    add botton on GroupBoard

--- client/src/components/GroupBoard.js
+++ client/src/components/GroupBoard.js
@@ -1,7 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
-import BackNavigation from "./BackNavigation";
 import withLanguage from "./LanguageContext";
+import Fab from "@material-ui/core/Fab";
+
+
+const styles = {
+  add: {
+    position: "absolute",
+    right: 0,
+    bottom: 0,
+    height: "5rem",
+    width: "5rem",
+    borderRadius: "50%",
+    border: "solid 0.5px #999",
+    backgroundColor: "#ff6f00",
+    zIndex: 100,
+    fontSize: "2rem"
+  }
+};
 
 
 class GroupBoard extends React.Component {
@@ -16,9 +32,22 @@ class GroupBoard extends React.Component {
     const { group } = this.props;
 
     return (
-      <React.Fragment>
-
-      </React.Fragment>
+      <div
+        className="row no-gutters"
+        style={{
+          bottom: "8rem",
+          right: "7%",
+          zIndex: 100,
+          position: "fixed"
+        }}
+      >
+        <Fab
+          color="primary"
+          style={styles.add}            
+        >
+          <i className="fas fa-plus" />
+        </Fab>
+      </div>
     );
   }
 }
